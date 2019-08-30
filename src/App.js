484cf96e0e2f9a06ab1a3a9c4be4332f83dac82d
Sophonias Isaac <sophonias8@gmail.com>
2019-08-30 18:41:14 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+
+//props simply come out of state
+// state is simply and obeject, an object that describes an application
+//state
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+  onSearchChange(event) {
+    console.log(event.target.value);
+  }
+
+
+
+  render() {
+  return (
+    <div className='tc'>
+    <h1> RoboPals</h1>
+    <SearchBox searchChange={this.onSearchChange} />
+  <CardList robots = {this.state.robots}/>
+
+  </div>
+);
+}
+}
+
+export default App;
